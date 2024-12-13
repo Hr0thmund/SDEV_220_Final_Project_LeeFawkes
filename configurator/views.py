713ba5b35f9b094a56923b23ee16a0e944763859
root <root@lemnos.digital-oasis.org>
2024-12-13 08:23:36 +0000

    Minor evolution of Django project setup.

--- configurator/views.py
+++ configurator/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def main_form(request):
+    return HttpResponse("<h1>This is the Web Configurator. Be amazed.</h1>")
+
+
